01525908a920be1ab28bbb512e939c467618aa6e
pieter willems <willems.pjn@gmail.com>
2023-01-24 23:57:23 +0100

    added CLI and program will now completely randomly create puzzles.

--- puzzle_generator/puzzle_generator.py
+++ puzzle_generator/puzzle_generator.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 import random
-
+import argparse
+import sys
 
 def selfmade_transpose(matrix):
     temp=matrix.copy()
@@ -15,7 +16,7 @@ def selfmade_transpose(matrix):
         i+=1
 
 
-def shift_colours(colour_matrix):
+def shift_colours(colour_matrix,shift):
     temp=np.array([[0,0,0],[0,0,0],[0,0,0]])
 
     #shift second row
@@ -23,22 +24,20 @@ def shift_colours(colour_matrix):
     while i<3:
         temp[i]=colour_matrix[1][i]
         i+=1
-    print(temp)
-    temp=np.roll(temp,-1,axis=0)
-    print(temp)
+    temp=np.roll(temp,shift,axis=0)
     i=0
     while i<3:
         colour_matrix[1][i]=temp[i]
         i+=1
 
     #shift third row
-    temp=np.roll(temp,-1,axis=0)
+    temp=np.roll(temp,shift,axis=0)
     i=0
     while i<3:
         colour_matrix[2][i]=temp[i]
         i+=1
 
-def shift_shapes(shape_matrix):
+def shift_shapes(shape_matrix,shift):
     temp=np.zeros((3,200,200,3))
     
     #shift second row
@@ -47,7 +46,7 @@ def shift_shapes(shape_matrix):
        
         temp[i]=shape_matrix[i][1]
         i+=1
-    temp=np.roll(temp,-1,axis=0)
+    temp=np.roll(temp,shift,axis=0)
    
 
     i=0
@@ -56,7 +55,7 @@ def shift_shapes(shape_matrix):
         i+=1
     
     #shift third row
-    temp=np.roll(temp,-1,axis=0)
+    temp=np.roll(temp,shift,axis=0)
     i=0
     while i<3:
         shape_matrix[i][2]=temp[i]
@@ -106,16 +105,8 @@ def draw_puzzle(shape_matrix,colour_matrix):
 
     return puzzle
 
-def template_switch(x,triangle_temp,circle_temp,square_temp):
-    if x==1:
-        return triangle_temp
-    elif x==2:
-        return circle_temp
-    else:
-        return square_temp
 
-
-def colour_puzzles():
+def build_puzzle(choices):
     colour_palette=np.array([[0,0,255],[0,255,255],[0,255,0],[255,255,0],[255,0,0],[255,0,255]])
     #red, yellow,green,cyan,blue and magenta in BGR 
 
@@ -124,22 +115,30 @@ def colour_puzzles():
     colour_order=np.zeros([3,3])
     
     x1=random.choice(colour_palette)
-    colour_order[0]=x1
-    while(1):
+
+    #make the colour order one colour if chosen by user argument
+    if(choices.one_colour):
+        colour_order[0]=x1
+        colour_order[1]=x1
+        colour_order[2]=x1
+
+    #otherwise the colour order is random
+    else:
+        colour_order[0]=x1
+        while(1):
       
-        x2=random.choice(colour_palette)
-        if np.array_equiv(x1,x2)==False:
-            break
-    colour_order[1]=x2
-    while(1):
-        x3=random.choice(colour_palette)
-        if np.array_equiv(x3,x1)==False and np.array_equiv(x3,x2)==False:
-            break
-    colour_order[2]=x3
+            x2=random.choice(colour_palette)
+            if np.array_equiv(x1,x2)==False:
+                break
+        colour_order[1]=x2
+        while(1):
+            x3=random.choice(colour_palette)
+            if np.array_equiv(x3,x1)==False and np.array_equiv(x3,x2)==False:
+                break
+        colour_order[2]=x3
+
+    #create a 3x3 matrix of the chosen colours
     colour_matrix=np.array([colour_order,colour_order,colour_order])
-    #shift_colours(colour_matrix)
-    
-   
 
     #create triangle template
     triangle_temp=np.ones((200,200,3),np.uint8)*255
@@ -161,24 +160,71 @@ def colour_puzzles():
     shapes=[triangle_temp,circle_temp,square_temp]
     shape_order=random.sample(shapes,3)
     
-     #create a shape matrix from randomly chosen shapes
+    #create a shape matrix from randomly chosen shapes
     shape_matrix=np.ndarray((3,3,200,200,3))
-    i=0
-    j=0
-    while (i<3):
-        while (j<3):
-            shape_matrix[i][j]=shape_order[i]
-            j+=1
+    #make all the shapes in the shape matrix the same if chosen by user argument
+    if(choices.one_shape):
+        i=0
         j=0
-        i+=1
+        while (i<3):
+            while (j<3):
+                shape_matrix[i][j]=shape_order[0]
+                j+=1
+            j=0
+            i+=1
+    #otherwise the shapes will be random
+    else:
+        i=0
+        j=0
+        while (i<3):
+            while (j<3):
+                shape_matrix[i][j]=shape_order[i]
+                j+=1
+            j=0
+            i+=1
 
+    #change colour and shape matrix based on choices
+    if len(sys.argv)>1:
+        print("no")
+        if choices.colourshift is not None and choices.one_colour is False:
+            shift_colours(colour_matrix,choices.colourshift)
+        if choices.shapeshift is not None and choices.one_shape is False:
+            shift_shapes(shape_matrix,choices.shapeshift)
+        if choices.transpose_colour:
+            selfmade_transpose(colour_matrix)
+        if choices.transpose_shapes:
+            selfmade_transpose(shape_matrix)
+    elif(not choices.one_colour and not choices.one_shape):
+        print("yes")
+        shift_colours(colour_matrix,random.choice([-2,-1,0,1,2]))
+        shift_shapes(shape_matrix,random.choice([-2,-1,0,1,2]))
+        if(bool(random.getrandbits(1))):
+            selfmade_transpose(colour_matrix)
+        if(bool(random.getrandbits(1))):
+            selfmade_transpose(shape_matrix)
     puzzle=draw_puzzle(shape_matrix,colour_matrix)
     
     cv2.imshow("image1",np.squeeze(puzzle))
     cv2.waitKey()
 
 def main():
-    colour_puzzles()
+    #parser for user arguments
+    parser=argparse.ArgumentParser(description='choose what kind of random puzzles you want with the help of arguments. If none are given, they will be random.')
+    parser.add_argument("--shift_colours",type=int,choices=[-2,-1,1,2],dest="colourshift",help="decide how the colours will be shifted in the puzzle. Negative values will shift to the left, positive values to the right.")
+    parser.add_argument("--shift_shapes",type=int,choices=[-2,-1,1,2],dest="shapeshift",help="decide how the shapes will be shifted in the puzzle. Negative values will shift to the left, positive values to the right.")
+    parser.add_argument("--one_colour",action="store_true",help="the shapes of the puzzle will consist of one colour instead of three random colours")
+    parser.add_argument("--one_shape",action="store_true",help="the shapes of the puzzle will be one type of shape instead of three random shapes") 
+    parser.add_argument("--transpose_colour",action="store_true",help="will transpose the colours of the puzzle ")
+    parser.add_argument("--transpose_shapes",action="store_true",help="will transpose the shapes of the puzzle")
+    x=None
+    parsed_args=parser.parse_args()
+    if parsed_args.colourshift is not None:
+        print("hey amigo")
+    if not len(sys.argv) > 1:
+        print(len(sys.argv))
+    parsed_args=parser.parse_args()
+    print(parsed_args)
+    build_puzzle(parsed_args)
 
 
 if __name__ == '__main__':
